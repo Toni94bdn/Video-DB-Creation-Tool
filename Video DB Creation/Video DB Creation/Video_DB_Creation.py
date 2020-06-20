ed8f4677b6d7fae7c83043801aafc259e8639db9
Toni94bdn <toni.forcada.sans@gmail.com>
2020-06-20 13:44:28 +0200

    json + try catch
    Settings Json deserialize
    Try catch blocks

---
+++ Video DB Creation/Video DB Creation/Video_DB_Creation.py
@@ -0,0 +1,19 @@
+
+import cv2
+import json
+import os
+import sys
+
+
+try:
+
+    with open(os.path.join(os.path.dirname(__file__),"Settings\settings.json")) as json_file:
+        config = json.load(json_file)['config']
+
+        
+
+
+    
+except Exception as e:
+    print("Error: "+str(e))
+
