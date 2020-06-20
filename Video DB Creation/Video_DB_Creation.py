23fc055285c563acabf4c0a325bce6a23f4ba0bc
Toni94bdn <toni.forcada.sans@gmail.com>
2020-06-20 18:54:29 +0200

    New Folder org.
    Some folder have been removed.

--- Video DB Creation/Video_DB_Creation.py
+++ Video DB Creation/Video_DB_Creation.py
@@ -10,10 +10,9 @@ try:
     with open(os.path.join(os.path.dirname(__file__),"Settings\settings.json")) as json_file:
         config = json.load(json_file)['config']
 
-        
+       
 
 
     
 except Exception as e:
     print("Error: "+str(e))
-
